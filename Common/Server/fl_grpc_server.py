b01c899a5ef84d72e3f82948b6f8440a20e2358d
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2021-11-08 23:56:30 +0100

    Update fl_grpc_server.py
    some comments added

--- Common/Server/fl_grpc_server.py
+++ Common/Server/fl_grpc_server.py
@@ -17,24 +17,25 @@ class FlGrpcServer(FL_GrpcServicer):
         super(FlGrpcServer, self).__init__()
         self.config = config
 
+    # multi threads
     def process(self, dict_data, handler):
         global num, data_ori, data_upd
 
         data_ori.update(dict_data)
 
-        con.acquire()
+        con.acquire() # request the lock
         num += 1
         if num < self.config.num_workers:
-            con.wait()
+            con.wait() # wait for the awake
         else:
             rst = [data_ori[k] for k in sorted(data_ori.keys())]
             rst = np.array(rst).flatten()
             data_upd = handler(data_in=rst)
             data_ori = {}
             num = 0
-            con.notifyAll()
+            con.notifyAll() # wake up all waiting threads
 
-        con.release()
+        con.release() # release the lock
 
         return data_upd
 
