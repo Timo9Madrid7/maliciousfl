19fc70d89e00c7b43db0f5c5c71f64b81d8b0f19
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2021-11-25 15:58:39 +0100

    inference adaptive clipping

--- start_client.bat
+++ start_client.bat
@@ -1,6 +1,6 @@
-python clear_dense_client_record.py --id 1 | ^
-python clear_dense_client_record.py --id 0
+python clearAdaclipping_client_record.py --id 1 | ^
+python clearAdaclipping_client_record.py --id 0
 
-@REM for /l %%i in (1,1,9) do start /b python clear_comb_client.py --id %%i
-@REM python clear_comb_client.py --id 0
+@REM for /l %%i in (1,1,9) do start /b python clearAdaclipping_client.py --id %%i
+@REM python clearAdaclipping_client.py --id 0
 @REM rundll32 user32.dll,MessageBeep
