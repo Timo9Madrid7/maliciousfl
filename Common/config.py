67222ae198e4feb3a5edfc5e3d6d7951ed04f005
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2021-11-25 15:57:57 +0100

    Update config.py

--- Common/config.py
+++ Common/config.py
@@ -30,9 +30,10 @@ llr = 0.01 # local learning rate
 
 # adaptive clipping parameters
 initClippingBound = 1 # initial clipping bound
+betta = 0.1 # last round gradient weight
 blr = 0.2 # clipping bound learning rate
 gamma = 0.5 # non-clipping ratio {0.1, 0.3, 0.5, 0.7, 0.9}
 # b_noise = num_workers/20 # noise standard deviation added to counts (from server side)
 b_noise = 1.5 # (from client side) [1.7823 ~ (e,10^-5)DP]
-z_multiplier = 2 # {0, 0.01, 0.03, 0.1}
+z_multiplier = 0.01 # {0, 0.01, 0.03, 0.1}
 
