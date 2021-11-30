7a3b1d8f625aae4d94d6756750108370747cba78
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2021-11-30 18:08:52 +0100

    do some tests

--- clear_comb_client.py
+++ clear_comb_client.py
@@ -53,11 +53,11 @@ class ClearDenseClient(WorkerBaseDitto):
             return gradients + np.random.normal(0, grad_noise, size=gradients.shape), 1 + np.random.normal(0,b_noise)
 
     def update(self):
-        if self.client_id < 10:
+        if self.client_id < 6:
             # clipping gradients before upload to the server
              gradients, b = self.adaptiveClipping(super().get_gradients())
         else:
-            # malicious client when id>=10
+            # malicious client when id>= threshold
              gradients, b = np.random.normal(0, 0.1, self._grad_len).tolist(), 1
 
         # upload local gradients and clipping indicator
--- start_client.bat
+++ start_client.bat
@@ -1,6 +1,6 @@
-python clear_comb_client.py --id 1 | ^
-python clear_comb_client.py --id 0
+@REM python clear_comb_client.py --id 1 | ^
+@REM python clear_comb_client.py --id 0
 
-@REM for /l %%i in (1,1,9) do start /b python clearAdaclipping_client.py --id %%i
-@REM python clearAdaclipping_client.py --id 0
-@REM rundll32 user32.dll,MessageBeep
+for /l %%i in (1,1,9) do start /b python clear_comb_client.py --id %%i
+python clear_comb_client.py --id 0
+rundll32 user32.dll,MessageBeep
