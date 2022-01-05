8d3794ab42014e721ac16dd3970a668dd2c385b5
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-01-05 23:13:21 +0100

    Update start_client.bat

--- start_client.bat
+++ start_client.bat
@@ -1,6 +1,10 @@
-@REM python clear_comb_client.py --id 1 | ^
-@REM python clear_comb_client.py --id 0
+@REM python clear_comb_client_FedAvg.py --id 1 | ^
+@REM python clear_comb_client_FedAvg.py --id 0
 
 for /l %%i in (1,1,9) do start /b python clear_comb_client_FedAvg.py --id %%i
 python clear_comb_client_FedAvg.py --id 0
 rundll32 user32.dll,MessageBeep
+
+@REM for /l %%i in (1,1,9) do start /b python clear_comb_client.py --id %%i
+@REM python clear_comb_client.py --id 0
+@REM rundll32 user32.dll,MessageBeep
