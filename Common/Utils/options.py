0852ea324029e188f64d0f64159727b6d8fec4e2
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-01-18 21:32:45 +0100

    Update options.py

--- Common/Utils/options.py
+++ Common/Utils/options.py
@@ -7,7 +7,7 @@ def args_parser():
     # parser.add_argument('--num_workers', type=int, default=10, help="number of users: N")
     # parser.add_argument('--E', type=int, default=1, help="the number of local updates: E")
     parser.add_argument('--batch_size', type=int, default=128, help="local batch size: B")
-    parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
+    parser.add_argument('--lr', type=float, default=0.1, help="learning rate")
     parser.add_argument('--momentum', type=float, default=0.9, help="SGD momentum (default: 0.5)")
     parser.add_argument('--id', type=int, default=0, help='thread id (default: 0)')
     parser.add_argument('--path', type=str, default='./Data/MNIST/', help="dataset")
