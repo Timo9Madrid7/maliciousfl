1abeab749478be92a20a4fd264be199da3afe132
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-06 15:36:21 +0100

    better model info

--- model_prepare.py
+++ model_prepare.py
@@ -5,6 +5,7 @@ from Common import config
 import torch
 import torchvision.models as models
 import os
+from torchsummary import summary
 
 
 
@@ -15,29 +16,30 @@ model = LeNet()
 #model = ResNet(BasicBlock, [3,3,3])
 torch.save(model.state_dict(), PATH)
 
-if not os.path.exists('./Model/Local_Models'):
-    os.makedirs('./Model/Local_Models')
+if not os.path.exists('./Model/LeNet/Local_Models'):
+    os.makedirs('./Model/LeNet/Local_Models')
 for i in range(config.total_number_clients):
     torch.save(model.state_dict(), config.local_models_path+str(i))
 
-model_load = LeNet()
-#model_load = models.resnet18()
-#model_load = ResNet(BasicBlock, [3,3,3])
-model_load.load_state_dict(torch.load(PATH))
-print(model_load)
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+data_shape = (1,28,28)
+temp_model = summary(model, data_shape)
 
-
-
-for name in model.state_dict():
-    print(name)
-layerslen = []
-for p in model.parameters():
-    if p.requires_grad:
-     layerslen.append(p.numel())
-layerslen = [sum(layerslen[:i + 1]) for i in range(len(layerslen))]
-layerslen = layerslen[:-1]
-print(layerslen)
-print(len(layerslen))
-print('parameters_count:',count_parameters(model))
+# # Model info
+# model_load = LeNet()
+# model_load = models.resnet18()
+# model_load = ResNet(BasicBlock, [3,3,3])
+# model_load.load_state_dict(torch.load(PATH))
+# print(model_load)
+# def count_parameters(model):
+#     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+# for name in model.state_dict():
+#     print(name)
+# layerslen = []
+# for p in model.parameters():
+#     if p.requires_grad:
+#      layerslen.append(p.numel())
+# layerslen = [sum(layerslen[:i + 1]) for i in range(len(layerslen))]
+# layerslen = layerslen[:-1]
+# print(layerslen)
+# print(len(layerslen))
+# print('parameters_count:',count_parameters(model))
