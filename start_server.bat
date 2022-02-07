ea42479af9b9a4208fa2519d76a59b0345686d0e
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-07 23:37:08 +0100

    deleted grpc relevance

--- start_server.bat
+++
@@ -1,8 +0,0 @@
-python model_prepare.py
-python fl_server_thread.py
-@REM python clear_comb_server.py
-@REM python clearAdaclipping_server.py
-@REM python clearDitto_server.py
-@REM python clearDittoHdbscan_server.py
-@REM python clear_dense_server.py
-@REM python clearflguard_server.py
