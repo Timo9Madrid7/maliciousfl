c82f80453ccfaa1d7a8427028cc8c356205ab4fc
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-09 00:37:39 +0100

    added generative model

--- model_prepare.py
+++ model_prepare.py
@@ -1,14 +1,18 @@
 from Common.Model.LeNet import LeNet
 from Common.Model.ResNet import ResNet, BasicBlock
+from Common.Model.Generator import Generator
 from Common.Utils.data_loader import load_data_mnist
-from Common import config
+from OfflinePack import offline_config as config
 import torch
 import torchvision.models as models
 import os
 from torchsummary import summary
 
-
-
+if config.reconstruct_inference:
+    G_model = Generator()
+    if not os.path.exists('./Model/LeNet'):
+        os.makedirs('./Model/LeNet')
+    torch.save(G_model.state_dict(), './Model/LeNet/InferModelG')
 
 if config.Model == "LeNet":
     model = LeNet()
