84c55d0046af672b3357942128632e552c8926b5
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-19 12:38:33 +0100

    updates

--- Common/Utils/data_malicious.py
+++ Common/Utils/data_malicious.py
@@ -5,30 +5,30 @@ import pdb
 def poison_data_mnist(path=None):
     for id in range(10):
         data = torch.load(path+'/'+'mnist_train_'+str(id)+'_.pt')
-        posion_data = []
+        poison_data = []
         for i in range(len(data)):
-            posion_data.append((data[i][0], 9 - data[i][1]))
-        torch.save(posion_data, path +'/' + 'mnist_train_posioned' + str(id) +'_.pt')
-    print("mnist posioned end")
+            poison_data.append((data[i][0], 9 - data[i][1]))
+        torch.save(poison_data, path +'/' + 'mnist_train_poisoned' + str(id) +'_.pt')
+    print("mnist poisoned end")
 
 
 def poison_data_cifar10(path=None):
     for id in range(10):
         data = torch.load(path+'/'+'cifar10_train_'+str(id)+'_.pt')
-        posion_data = []
+        poison_data = []
         for i in range(len(data)):
-            posion_data.append((data[i][0], 9 - data[i][1]))
-        torch.save(posion_data, path +'/' + 'cifar10_train_posioned' + str(id) +'_.pt')
-    print("posioned end")
+            poison_data.append((data[i][0], 9 - data[i][1]))
+        torch.save(poison_data, path +'/' + 'cifar10_train_poisoned' + str(id) +'_.pt')
+    print("poisoned end")
 
 def poison_data_fmnist(path=None):
     for id in range(10):
         data = torch.load(path+'/'+'fmnist_train_'+str(id)+'_.pt')
-        posion_data = []
+        poison_data = []
         for i in range(len(data)):
-            posion_data.append((data[i][0], 9 - data[i][1]))
-        torch.save(posion_data, path +'/' + 'fmnist_train_posioned' + str(id) +'_.pt')
-    print("fmnist posioned end")
+            poison_data.append((data[i][0], 9 - data[i][1]))
+        torch.save(poison_data, path +'/' + 'fmnist_train_poisoned' + str(id) +'_.pt')
+    print("fmnist poisoned end")
 
 
 
