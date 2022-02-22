2c34bdaadec313f5ca6901bfe4c2c9b33c06c24e
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-22 15:06:28 +0100

    fixed runtime error: aggregation_s2pc

--- Crypto/s2pcCrypTen.py
+++ Crypto/s2pcCrypTen.py
@@ -18,16 +18,22 @@ class S2PC():
                 distance_matrix[i][j] = distance_matrix[j][i] = (grad_share[i]*grad_share[j]).sum().get_plain_text().item()
         return distance_matrix
 
-    # !: runtime error
-    @mpc.run_multiprocess(world_size=2)
-    def aggregation_s2pc(self, grads_secrete:list, norms_secrete:list, bs_secrete:list, benign_id:list):
-        grads_sum = 0
-        bs_sum = 0
-        for _id in benign_id:
-            grads_sum += (grads_secrete[_id]*norms_secrete[_id])
-            if bs_secrete[_id] > 0:
-                bs_sum += bs_secrete[_id]
-        return (grads_sum/len(benign_id)).get_plain_text(), (bs_sum/len(benign_id)).get_plain_text().item()
+    def aggregation_s2pc(grads_secrete:list, norms_secrete:list, bs_secrete:list, benign_id:list):
+        @mpc.run_multiprocess(world_size=2)
+        def aggregation(grads_secrete:list, norms_secrete:list, bs_secrete:list, benign_id:list):
+            grads_share = crypten.cryptensor(grads_secrete, precision=24)
+            norms_share = crypten.cryptensor(norms_secrete, precision=24)
+            bs_share = crypten.cryptensor(bs_secrete)
+            grads_sum = 0
+            for _id in benign_id:
+                grads_sum += (grads_share[_id]*norms_share[_id])
+            bs_sum = (bs_share[benign_id] > 0).sum().get_plain_text().item()
+            grads_sum = grads_sum.get_plain_text()
+            crypten.save(grads_sum, "./temp.pt")
+            return bs_sum
+        bs_sum, _ = aggregation(grads_secrete, norms_secrete, bs_secrete, benign_id)
+        grads_sum = crypten.load('./temp.pt')
+        return grads_sum, bs_sum
     
 
     
