f72e0458acb1565bee7b7622ff75809b2f77b34f
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-22 16:34:03 +0100

    mean-subtraction cosine distance

--- Crypto/s2pcCrypTen.py
+++ Crypto/s2pcCrypTen.py
@@ -1,5 +1,6 @@
 import crypten
 import torch
+import numpy as np
 import crypten.mpc as mpc
 import crypten.communicator as comm
 
@@ -12,10 +13,11 @@ class S2PC():
     @mpc.run_multiprocess(world_size=2)
     def cosinedist_s2pc(self, grads_secrete:list):
         grad_share = crypten.cryptensor(grads_secrete, precision=30)
+        grad_share_mean = grad_share.mean(axis=0)
         distance_matrix = [[0 for _ in range(len(grads_secrete))] for _ in range(len(grads_secrete))]
         for i in range(len(grads_secrete)):
             for j in range(i+1, len(grads_secrete)):
-                distance_matrix[i][j] = distance_matrix[j][i] = (grad_share[i]*grad_share[j]).sum().get_plain_text().item()
+                distance_matrix[i][j] = distance_matrix[j][i] = 1 - ((grad_share[i]-grad_share_mean)*(grad_share[j]-grad_share_mean)).sum().get_plain_text().item()
         return distance_matrix
 
     def aggregation_s2pc(self, grads_secrete:list, norms_secrete:list, bs_secrete:list, benign_id:list):
@@ -29,11 +31,11 @@ class S2PC():
                 grads_sum += (grads_share[_id]*norms_share[_id])
             bs_sum = (bs_share[benign_id] > 0).sum().get_plain_text().item()
             grads_sum = grads_sum.get_plain_text()
-            crypten.save(grads_sum, "./temp.pt")
+            np.savetxt("./temp.txt", grads_sum)
             return bs_sum
         bs_sum, _ = aggregation(grads_secrete, norms_secrete, bs_secrete, benign_id)
-        grads_sum = crypten.load('./temp.pt')
-        return grads_sum.div(len(benign_id)), bs_sum/len(benign_id)
+        grads_sum = np.loadtxt('./temp.txt')
+        return grads_sum/(len(benign_id)), bs_sum/len(benign_id)
     
 
     
