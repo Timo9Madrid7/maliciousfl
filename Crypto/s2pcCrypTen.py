44c0a4720d08d7a4f3fc0372599b8edf5b5cf1ba
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-02-22 18:17:31 +0100

    added debugging check

--- Crypto/s2pcCrypTen.py
+++ Crypto/s2pcCrypTen.py
@@ -11,13 +11,22 @@ class S2PC():
         torch.set_num_threads(1)
 
     @mpc.run_multiprocess(world_size=2)
-    def cosinedist_s2pc(self, grads_secrete:list):
+    def cosinedist_s2pc(self, grads_secrete:list, correctness_check=False):
         grad_share = crypten.cryptensor(grads_secrete, precision=30)
         grad_share_mean = grad_share.mean(axis=0)
         distance_matrix = [[0 for _ in range(len(grads_secrete))] for _ in range(len(grads_secrete))]
         for i in range(len(grads_secrete)):
             for j in range(i+1, len(grads_secrete)):
                 distance_matrix[i][j] = distance_matrix[j][i] = 1 - ((grad_share[i]-grad_share_mean)*(grad_share[j]-grad_share_mean)).sum().get_plain_text().item()
+        
+        if correctness_check:
+            distance_matrix_compare = self.cosinedist_correctness_check(grads_secrete)
+            for i in range(len(grads_secrete)):
+                print("| ", end="")
+                for j in range(len(grads_secrete)):
+                    print("%.6f | "%(np.abs(distance_matrix[i][j]-distance_matrix_compare[i][j])), end="")
+                print()
+        
         return distance_matrix
 
     def aggregation_s2pc(self, grads_secrete:list, norms_secrete:list, bs_secrete:list, benign_id:list):
@@ -37,5 +46,12 @@ class S2PC():
         grads_sum = np.loadtxt('./temp.txt')
         return grads_sum/(len(benign_id)), bs_sum/len(benign_id)
     
-
+    def cosinedist_correctness_check(self, grads_secrete:list):
+        grads_secrete = np.array(grads_secrete)
+        grads_secrete_mean = grads_secrete.mean(axis=0)
+        distance_matrix = [[0 for _ in range(len(grads_secrete))] for _ in range(len(grads_secrete))]
+        for i in range(len(grads_secrete)):
+            for j in range(i+1, len(grads_secrete)):
+                 distance_matrix[i][j] = distance_matrix[j][i] = 1 - ((grads_secrete[i]-grads_secrete_mean)*(grads_secrete[j]-grads_secrete_mean)).sum()
+        return distance_matrix
     
