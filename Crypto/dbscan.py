c648f09b942632cf533550874c2239adc9125bbb
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-03-02 17:20:56 +0100

    MPCTensor cannot support torch operations

--- Crypto/dbscan.py
+++ Crypto/dbscan.py
@@ -86,79 +86,21 @@ class EncDBSCAN:
         
         self.labels_, self.core_sample_indices_ = None, None
 
-    def _neighbor_points(self):
-        pointGroup = []
-        for i in  range(len(self.data)):
-            indices = self.s2pc.comparisonReconstruct_s2pc((self.data - self.data[i]).square().sum(axis=1) <= self.radius)
-            # indices = (self.data - self.data[i]).square().sum(axis=1) <= self.radius
-            pointGroup.append(numpy.where(indices)[0].tolist())
-        return pointGroup
+    # def _neighbor_points(self):
+    #     pointGroup = []
+    #     for i in  range(len(self.data)):
+    #         indices = self.s2pc.comparisonReconstruct_s2pc((self.data - self.data[i]).square().sum(axis=1) <= self.radius)
+    #         # indices = (self.data - self.data[i]).square().sum(axis=1) <= self.radius
+    #         pointGroup.append(numpy.where(indices)[0].tolist())
+    #     return pointGroup
 
-    def fit(self, data:torch.Tensor):
-        self.data = data 
-        self.numPoints = len(self.data)
-        pointLabel = [self._unassigned] * self.numPoints
-        pointGroup = self._neighbor_points()
-        corePoint = []
-        nonCorePoint = []
-            
-        for i in range(len(pointGroup)):
-            if len(pointGroup[i]) >= self.minPoints:
-                pointLabel[i] = self._core
-                corePoint.append(i)
-            else:
-                nonCorePoint.append(i)
-        
-        for i in nonCorePoint:
-            for j in pointGroup[i]:
-                if j in corePoint: 
-                    pointLabel[i] = self._border
-                    break 
-                            
-        curClusterID = 0
-        for i in range(len(pointLabel)):
-            coreGroup = []
-            if pointLabel[i] == self._core:
-                pointLabel[i] = curClusterID
-                for j in pointGroup[i]:
-                    if pointLabel[j] == self._core:
-                        coreGroup.append(j)
-                    pointLabel[j] = curClusterID
-                    
-                while coreGroup != []:
-                    neighbors = pointGroup[coreGroup.pop()]
-                    for k in neighbors:
-                        if pointLabel[k] == self._core:
-                            coreGroup.append(k)
-                        pointLabel[k] = curClusterID # It is being said that DBSCAN is not consistent on the border points and depends on which cluster it assigns the point to first.
-            
-                curClusterID += 1
-            
-        self.labels_ = numpy.array(pointLabel, dtype=int)
-        self.core_sample_indices_ = numpy.array(coreGroup, dtype=int)
-        return self
-
-    # def _l2_distance(self, a, b):
-    #     return (a-b).square().sum()
-    
-    # def _neighbor_points(self, pointID):
-    #     neighbors = []
-    #     for i in range(self.numPoints):
-    #         # if self._l2_distance(self.data[pointID], self.data[i]) <= self.radius:
-    #         if self.s2pc.notLargerThan_s2pc(self._l2_distance(self.data[pointID], self.data[i]), self.radius):
-    #             neighbors.append(i)
-    #     return neighbors
-    
     # def fit(self, data:torch.Tensor):
     #     self.data = data 
     #     self.numPoints = len(self.data)
     #     pointLabel = [self._unassigned] * self.numPoints
-    #     pointGroup = []
+    #     pointGroup = self._neighbor_points()
     #     corePoint = []
     #     nonCorePoint = []
-        
-    #     for i in range(self.numPoints):
-    #         pointGroup.append(self._neighbor_points(i))
             
     #     for i in range(len(pointGroup)):
     #         if len(pointGroup[i]) >= self.minPoints:
@@ -195,6 +137,70 @@ class EncDBSCAN:
     #     self.labels_ = numpy.array(pointLabel, dtype=int)
     #     self.core_sample_indices_ = numpy.array(coreGroup, dtype=int)
     #     return self
+
+    # def _l2_distance(self, a, b):
+    #     return (a-b).square().sum()
+    def _l2_distance(self, a:list, b:list):
+        assert len(a) == len(b)
+        sum_square = 0
+        for i in range(len(a)):
+            sum_square += (a[i] - b[i]).square()
+        return sum_square
+    
+    def _neighbor_points(self, pointID):
+        neighbors = []
+        for i in range(self.numPoints):
+            # if self._l2_distance(self.data[pointID], self.data[i]) <= self.radius:
+            if self.s2pc.notLargerThan_s2pc(self._l2_distance(self.data[pointID], self.data[i]), self.radius):
+                neighbors.append(i)
+        return neighbors
+    
+    def fit(self, data:torch.Tensor):
+        self.data = data 
+        self.numPoints = len(self.data)
+        pointLabel = [self._unassigned] * self.numPoints
+        pointGroup = []
+        corePoint = []
+        nonCorePoint = []
+        
+        for i in range(self.numPoints):
+            pointGroup.append(self._neighbor_points(i))
+            
+        for i in range(len(pointGroup)):
+            if len(pointGroup[i]) >= self.minPoints:
+                pointLabel[i] = self._core
+                corePoint.append(i)
+            else:
+                nonCorePoint.append(i)
+        
+        for i in nonCorePoint:
+            for j in pointGroup[i]:
+                if j in corePoint: 
+                    pointLabel[i] = self._border
+                    break 
+                            
+        curClusterID = 0
+        for i in range(len(pointLabel)):
+            coreGroup = []
+            if pointLabel[i] == self._core:
+                pointLabel[i] = curClusterID
+                for j in pointGroup[i]:
+                    if pointLabel[j] == self._core:
+                        coreGroup.append(j)
+                    pointLabel[j] = curClusterID
+                    
+                while coreGroup != []:
+                    neighbors = pointGroup[coreGroup.pop()]
+                    for k in neighbors:
+                        if pointLabel[k] == self._core:
+                            coreGroup.append(k)
+                        pointLabel[k] = curClusterID # It is being said that DBSCAN is not consistent on the border points and depends on which cluster it assigns the point to first.
+            
+                curClusterID += 1
+            
+        self.labels_ = numpy.array(pointLabel, dtype=int)
+        self.core_sample_indices_ = numpy.array(coreGroup, dtype=int)
+        return self
 
 def plot_2Dresult(X, labels, core_sample_indices):
     import matplotlib.pyplot as plt
