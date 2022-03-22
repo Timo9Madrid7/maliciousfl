79c6f49926ba07567f4ec852fadb89b88391e57e
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2022-03-22 14:32:47 +0100

    debug
    - args error

--- fl_offline_sympc_v2.py
+++ fl_offline_sympc_v2.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     aggregator = AvgGradientHandler(config, global_model, device, test_iter)
 
     # secure two-party computation initialize
-    s2pc = S2PC(num_clients=config.num_workers)
+    s2pc = S2PC()
 
     print('model:', config.Model, 
     '| dpoff:', config._dpoff, '| grad_noise_sigma:', config.grad_noise_sigma, '| b_noise_std:', config.b_noise_std, '| clip_ratio:', config.gamma,
